3294928581d3bcdbfa5ded2f4ddf3265b8245a2b
MINT <pb1123love@gmail.com>
2022-10-15 19:25:36 +0900

    two sum rust 버전 FIN

--- Two Sum/two_sum/src/main.rs
+++ Two Sum/two_sum/src/main.rs
@@ -1,3 +1,22 @@
+struct Solution {}
+
+impl Solution {
+    fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+        let mut num_dict: HashMap<i32, i32> = HashMap::new();
+
+        for (idx, num) in nums.iter().enumerate() {
+            match num_dict.get(&(target - *num)) {
+                Some(value) => return vec![*value, idx as i32],
+                None => num_dict.insert(*num, idx as i32)
+            };
+        }
+        vec![]
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let result = Solution::two_sum(vec![3, 2, 4], 6);
+    println!("{:?}", result);
 }

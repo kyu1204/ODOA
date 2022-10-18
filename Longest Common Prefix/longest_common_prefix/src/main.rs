8f0aa3608de7a6773d1fb7322c9d69406341ec5e
MINT <pb1123love@gmail.com>
2022-10-18 22:39:01 +0900

    longest common prefix rust day 1

--- Longest Common Prefix/longest_common_prefix/src/main.rs
+++ Longest Common Prefix/longest_common_prefix/src/main.rs
@@ -1,3 +1,21 @@
+struct Solution {}
+
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        if strs.len() == 0 {
+            return String::from("")
+        }
+        else if strs.len() == 1 {
+            return strs.get(0).unwrap().
+        }
+
+
+        String::from("")
+    }
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }

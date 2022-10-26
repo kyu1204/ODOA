d8a60a1308d33e1c1a252fbc0c1f912f95b3eee9
MINT <pb1123love@gmail.com>
2022-10-26 17:11:30 +0900

    코드 정리

--- Palindrome integer/palindrome_integer/src/main.rs
+++ Palindrome integer/palindrome_integer/src/main.rs
@@ -6,11 +6,8 @@ impl Solution {
             return false
         }
         let digit_count = 1 + (x as f32).log10() as u32;
-        println!("digit_count: {}", digit_count);
         let half = (digit_count / 2) as usize;
-        println!("half: {}", half);
         let digits = (0..digit_count).map(|exp| x / 10_i32.pow(exp) % 10);
-        println!("digits: {:?}", digits);
         digits
             .clone()
             .take(half)
